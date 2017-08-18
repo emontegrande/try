2b10ee8ab41e0d561c27daeae810f913461f778c
emontegrande <montegrandeelouiseit1a@gmail.com>
2017-08-18 20:48:15 +0800

    try

--- js/isLogin.js
+++ js/isLogin.js
@@ -27,7 +27,7 @@ function isLogin(){
   			else if(account[i].txtName == inname && account[i].txtPass == inpassword){
     			console.log(account[i].txtNname);
     			console.log(account[i].txtPassword);
-    			window.open("/users");
+    			window.open("/");
     			break;
 			}
 
